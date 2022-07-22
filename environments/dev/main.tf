6e7e306ccfa511a63fca4c6c8e10e2307321e665
rajkartik <29058017rajkartik@users.noreply.github.com>
2022-07-22 15:47:19 +0530

    first commit

---
+++ environments/dev/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "gcs" {
+    bucket = "mydev-states-climate"
+    prefix = "cloud_build_samp"
+  }
+}
+module "vpc" {
+  source     = "../modules/net-vpc"
+  project_id = "automategcp"
+  name       = "my-network-cicd"
+  psa_config = {
+    ranges = { vpc-private-connect = "/20"
+     }
+    routes = null
+  }
+  
+
+#   subnets = [
+#     {
+#       ip_cidr_range = "10.0.0.0/24"
+#       name          = "subnet-1"
+#       private_ip_google_access=true
+#       region        = "europe-west1"
+#       secondary_ip_range = {
+#         pods     = "192.16.0.0/20"
+#         services = "192.168.0.0/24"
+#       }
+#     }
+#   ]
+ 
+}
