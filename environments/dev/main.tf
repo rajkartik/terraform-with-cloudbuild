12e7d4933446af8cbc0e58459d34d479aeab86eb
rajkartik <29058017rajkartik@users.noreply.github.com>
2022-07-22 16:40:47 +0530

    minor changes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,24 +8,24 @@ module "vpc" {
   source     = "../../modules/net-vpc"
   project_id = "automategcp"
   name       = "my-network-cicd-2"
-  psa_config = {
-    ranges = { vpc-private-connect-2 = "/20"
-     }
-    routes = null
-  }
+#   psa_config = {
+#     ranges = { vpc-private-connect-2 = "/20"
+#      }
+#     routes = null
+#   }
   
 
-#   subnets = [
-#     {
-#       ip_cidr_range = "10.0.0.0/24"
-#       name          = "subnet-1"
-#       private_ip_google_access=true
-#       region        = "europe-west1"
-#       secondary_ip_range = {
-#         pods     = "192.16.0.0/20"
-#         services = "192.168.0.0/24"
-#       }
-#     }
-#   ]
+  subnets = [
+    {
+      ip_cidr_range = "10.0.0.0/24"
+      name          = "subnet-1"
+      private_ip_google_access=true
+      region        = "europe-west1"
+      secondary_ip_range = {
+        pods     = "192.16.0.0/20"
+        services = "192.168.0.0/24"
+      }
+    }
+  ]
  
 }
