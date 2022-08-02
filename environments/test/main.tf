1e76af9459881851f92bf5b775b506818b6db020
rajkartik <29058017rajkartik@users.noreply.github.com>
2022-08-02 19:31:44 +0530

    backend changes

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "gcs" {
+    project_id="automate-357907"
     bucket = "mysamp-states-automatic"
     prefix = "cloud_build_samp"
+    impersonate_service_account = "196069267713@cloudbuild.gserviceaccount.com"
   }
 }
 module "vpc" {
